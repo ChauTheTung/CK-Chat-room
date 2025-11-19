53211ed97afde24ff9002b0a49af00d467edd4c6
Cao Hung Vi <email_cua_ban@example.com>
2025-11-19 18:23:47 +0700

    push websocketconfig , chatcontroller

---
+++ chat-app-java/src/main/java/com/multiroomchat/controller/ChatController.java
@@ -0,0 +1,39 @@
+package com.multiroomchat.controller;
+
+import com.multiroomchat.model.ChatMessage;
+import com.multiroomchat.model.MessageType;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class ChatController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        headerAccessor.getSessionAttributes().put("room", chatMessage.getRoom());
+        return chatMessage;
+    }
+}
+
+
